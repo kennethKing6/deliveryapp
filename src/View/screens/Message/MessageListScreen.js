39f2335048f320f8b88e848e4dd237bb84ae4d2e
kennethKing6 <kennethemmanuelziguy28@gmail.com>
2020-11-11 22:29:19 -0800

    messaging feature stage 2

--- src/View/screens/Message/MessageListScreen.js
+++ src/View/screens/Message/MessageListScreen.js
@@ -15,23 +15,27 @@ import {
     Image,
     TouchableOpacity
 } from 'react-native';
-import ChatScreen from './ChatScreen';
 
 
 
 
-export default class MessageListScreen extends Component {
+export default function MessageListScreen(props) {
     
    
-    render() { 
+    
         
         return (
 
-            <TouchableOpacity onPress = {() => {navigation.navigate("ChatScreen")}}>
+           <View>
+              <TouchableOpacity onPress = {() => {props.navigation.navigate("ChatScreen",{correspondence:"sOf5W0LrhXdvNGJbo9raJS8F3if1"})}}>
                 <Text style = {{fontSize:30}}>Kenneth</Text>
             </TouchableOpacity>
+            <TouchableOpacity onPress = {() => {props.navigation.navigate("ChatScreen",{correspondence:"QpcV2XFV8ZR3v1dOrS71aeGTxtv2"})}}>
+                <Text style = {{fontSize:30}}>Shahbek</Text>
+            </TouchableOpacity>
+           </View>
     );
-  }
+  
 }
 
 const styles = StyleSheet.create({
