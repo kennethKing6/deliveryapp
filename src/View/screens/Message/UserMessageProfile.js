8bc32b53b489ed5a80f465a2367d9aacc9dd4189
ShahbekMiru <shahbekmiru05@gmail.com>
2020-11-13 03:04:54 -0800

    added chat profiles

---
+++ src/View/screens/Message/UserMessageProfile.js
@@ -0,0 +1,21 @@
+const UserMessageProfile=[
+    {
+    "key" : "1",
+    "name" : "Kenneth",
+    "src" : require('../../../../src/assets/images/kenneth.jpg'),
+    "correspondence": "sOf5W0LrhXdvNGJbo9raJS8F3if1"
+  },
+  {
+  "key" : "2",
+  "name" : "Shahbek",
+  "src" : require('../../../../src/assets/images/shahbek.jpg'),
+  "correspondence": "QpcV2XFV8ZR3v1dOrS71aeGTxtv2"
+
+ 
+  },
+  
+  
+        
+ 
+]
+export default UserMessageProfile;
